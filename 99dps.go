3c3829e61e436f2f02db0db08afe42c923fc2c8d
Grant Tepper <grant.tepper@gmail.com>
2019-08-25 04:12:49 -0700

    begins implement gocui

--- 99dps.go
+++ 99dps.go
@@ -1,39 +1,36 @@
 package main
 
 import (
-	"99dps/input"
-	"99dps/loader"
-	"99dps/parser"
-	"99dps/session"
-	"fmt"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
+	"99dps/loader"
+	"99dps/session"
+	"99dps/parser"
+	"99dps/app"
 )
 
 var rwLock = sync.RWMutex{}
 
 func main() {
+
+
+	activeFile := loader.LoadFile()
+	sm := session.SessionManager{}
+
+	a := app.New()
+
+	go parser.DoParse(activeFile, &sm, &rwLock)
+
+	a.Loop()
+
+	/*
 	inputChan := make(chan string)
 	defer close(inputChan)
 
-	sigChan := make(chan os.Signal, 2)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sigChan
-		fmt.Println("\rShutting down..")
-		os.Exit(0)
-	}()
-
 	input.Help()
 
-	activeFile := loader.LoadFile()
-
-	sm := session.SessionManager{}
-
-	go parser.DoParse(activeFile, &sm, &rwLock)
 	go input.ScanInput(inputChan)
 
 	input.HandleInput(inputChan, &sm, &rwLock)
+	*/
 }
+
